958431b1927ee7b2e935d34422aca508175a9913
jedgell <edgellj@gmail.com>
2014-07-10 05:06:00 -0400

    Expands the CsvToSqlite\Framework\Exception class and starts implementing the MVC framework at the model.

--- src/CsvToSqlite/Framework/Exception.php
+++ src/CsvToSqlite/Framework/Exception.php
@@ -5,22 +5,52 @@ namespace CsvToSqlite\Framework;
 /**
  * Define a custom exception class (pretty much straight from php.net)
  */
-class Exception extends \Exception
-{
-    // Redefine the exception so message isn't optional
-    public function __construct($message, $code = 0, Exception $previous = null) {
-        // some code
+class Exception extends \Exception {
 
-        // make sure everything is assigned properly
+    protected $message_index = array(
+        0   => 'Undefined error.',
+        // Configuration exceptions.
+        100 => 'An invalid type was passed to the SQLite for configuration. Valid types are "memory" or "file".',
+        101 => 'A %s must be provided for SQLite databases of type "%s".',
+
+        // Programming exceptions.
+        200 => 'Required value %s not passed to class %s constructor.',
+        201 => 'Required value not passed to method %s',
+
+        // System compatability.
+        500 => 'Incompatible environment: extension "%s" not loaded.',
+    );
+
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, Exception $previous = NULL, array $arguments) {
+        $code = (int) $code;
+        if (is_null($message) && $code !== 0) {
+            $message = vsprintf($this->message_index[$code], $arguments);
+        }
         parent::__construct($message, $code, $previous);
     }
 
-    // custom string representation of object
     public function __toString() {
-        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        $message_template  = __CLASS__ . " [{$this->code}]: {$this->message}" . PHP_EOL;
+        $message_template .= "In file: " . $this->file . PHP_EOL;
+        $message_template .= "At line: " . $this->line . PHP_EOL;
+        return $message_template;
     }
 
-    public function customFunction() {
-        echo "A custom function for this type of exception\n";
+}
+
+
+function exceptionTest()
+{
+    try {
+        throw new Exception(null, 100, null, array('lolly pop', 'baby'));
+    }
+    catch (Exception $e) {
+        echo "Caught TestException ('{$e->getMessage()}')\n{$e}\n";
+    }
+    catch (Exception $e) {
+        echo "Caught Exception ('{$e->getMessage()}')\n{$e}\n";
     }
 }
+
+echo '<pre>' . exceptionTest() . '</pre>';
--- src/CsvToSqlite/Framework/Model.php
+++ src/CsvToSqlite/Framework/Model.php
@@ -7,6 +7,11 @@ use Exception;
 class Model {
 
   function __construct(array $configuration) {
+
+    if (!extension_loaded('PDO_SQLITE')) {
+      throw new Exception(null, 500, null, array('PDO_SQLITE'));
+    }
+
     switch ($configuration['type']) {
       case 'memory':
         $this->database = new PDO('sqlite::memory:');
@@ -14,16 +19,18 @@ class Model {
 
       case 'file':
         if (!isset($configuration['file_name']) || empty($configuration['file_name'])) {
-          throw new CsvToSqlite\Exception('A file name must be provided for SQLite databases of type "file".', 100);
+          throw new Exception(null, 101, null, array('file_name', 'file'));
         }
         if (!isset($configuration['file_path']) || empty($configuration['file_path'])) {
-          throw new CvsToSqlite\Exception('A file path must be provided for SQLite databases of type "file".', 101);
-
+          throw new Exception(null, 101, null, array('file_path', 'file'));
         }
-        $this->database = new PDO('sqlite:messaging.sqlite3');
+        if (!file_exists($configuration['file_path'] . DIRECTORY_SEPARATOR . $configuration['file_name']) && is_writable($configuration['file_path'])) {
+          $this->database = new PDO('sqlite:' . $configuration['file_path'] . DIRECTORY_SEPARATOR . $configuration['file_name']);
+        }
+        break;
 
       default:
-        # code...
+        throw new Exception(null, 100, null, null);
         break;
     }
 
